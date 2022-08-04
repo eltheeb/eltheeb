880ec99569fcb1513a626267e54f81278c9abee2
eltheeb <75144092+eltheeb@users.noreply.github.com>
2022-08-04 20:21:53 +0300

    Add files via upload

---
+++ ya.cpp
@@ -0,0 +1,21 @@
+#include "XMemoryTools.h"
+#include "obfuscate-me.h"
+#include <iostream>
+#include "base.h"
+#include "check.h"
+#include "shell.h"
+#include "is.h"
+//#include "byte.h"
+#include "agnos.h"
+//#include "bu4.h"
+//#include "gcl.h"
+//#include "cup.h"
+#include "f.h"
+   
+
+	
+				
+	
+
+	
+
